1bd5e2a19a1a287e663a71f4eea545be2f1e7faf
IgunOfficial <irfan_123@smkassalaambandung.sch.id>
2023-02-21 08:53:05 +0700

    day 1

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Latihan Flutter",
+      home : Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlue,
+          centerTitle: true,
+          title:Text("Latihan"),
+        ),
+        body: Center(
+          child: Text("Hallo Dunia",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+            backgroundColor: Colors.black12,
+          ),
+          ),
+        ),
+      ),
+    );
+  }
+}
